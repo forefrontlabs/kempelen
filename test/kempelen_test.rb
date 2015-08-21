93a845c3c92c3c370ad993df5ba25da077aaa75f
Brian Nelson <brian@forefrontlabs.com>
2015-08-21 16:04:01 -0500

    Adding tests for API client and base operation.

--- test/kempelen_test.rb
+++ test/kempelen_test.rb
@@ -4,12 +4,4 @@ class KempelenTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Kempelen::VERSION
   end
-
-  def test_it_does_not_something_useful
-    assert false
-  end
-
-  def test_it_does_something_useful
-    assert true
-  end
 end
